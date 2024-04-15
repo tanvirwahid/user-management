7fe3ce53454c2c55414396b32093294492433344
Tanvir <nevillongbottom33@gmail.com>
2024-04-15 17:44:01 +0600

    Add logout frontend

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -5,15 +5,21 @@
         </div>
         <div class="d-flex align-items-center">
             <div class="dropdown mx-3">
-                <button class="btn btn-transparent text-white dropdown-toggle" type="button"
-                    id="dropdownMenuButton">
+                <button class="btn btn-transparent text-white dropdown-toggle" type="button" id="dropdownMenuButton">
                     {{ Auth::user()->first_name }}
                 </button>
                 <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
                     <a class="dropdown-item" href="#">Action</a>
                     <a class="dropdown-item" href="#">Another action</a>
                     <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Logout</a>
+                    <div class="dropdown-item">
+                        <form action="{{ route('logout') }}" method="POST">
+                            @csrf
+                            <button class="btn btn-transparent" type="submit">
+                                Logout
+                            </button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
