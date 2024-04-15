37b73db5522bc43ff1841c5ec9f290eeb2284519
Tanvir <nevillongbottom33@gmail.com>
2024-04-15 17:44:09 +0600

    Some clean ups

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>@yield('title', 'User Manager')</title>
 
     <!-- Styles -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('verify', function () {
-    return view('auth.two-factor');
-});
+Route::group(
+    [
+        'middleware' => 'guest',
+    ],
+    function () {
+        Route::get('/register', [RegistrationController::class, 'create'])->name('registration.form');
+        Route::post('/register', [RegistrationController::class, 'store'])->name('register');
+        Route::get('check-email', [RegistrationController::class, 'checkIfEmailExists']);
+
+        Route::get('/login', [SessionController::class, 'showLoginPage'])->name('login.form');
+        Route::post('/login', [SessionController::class, 'login'])->name('login');
+    }
+);
+
 
 Route::group(
     [
@@ -50,18 +61,4 @@ Route::group(
         );
     }
 
-);
-
-Route::group(
-    [
-        'middleware' => 'guest',
-    ],
-    function () {
-        Route::get('/register', [RegistrationController::class, 'create'])->name('registration.form');
-        Route::post('/register', [RegistrationController::class, 'store'])->name('register');
-        Route::get('check-email', [RegistrationController::class, 'checkIfEmailExists']);
-
-        Route::get('/login', [SessionController::class, 'showLoginPage'])->name('login.form');
-        Route::post('/login', [SessionController::class, 'login'])->name('login');
-    }
 );
